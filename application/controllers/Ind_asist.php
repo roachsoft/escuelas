d0d41be69b7b657d25985158c16a09ff9752e8ce
RoachSoft <mmontania@gmail.com>
2015-11-10 01:21:04 -0300

    Se añaden cambios en el modulo indicadores de asistencia.

--- application/controllers/Ind_asist.php
+++ application/controllers/Ind_asist.php
@@ -26,10 +26,26 @@ class Ind_asist extends CI_Controller {
         $aula=$_POST['curso'];
         $actividad=$_POST['actividad'];
 
-        $data['totalRegistros'] = $this->Ind_asistModel->TotalRegistros($mes, $periodo, $aula, $actividad);
+        $totalAusentes = 0;
+        $totalPresente = 0;
+        $totalTardanza = 0;
 
-        
+        $data['totalRegistros'] = $this->Ind_asistModel->totalRegistros($mes, $periodo, $aula, $actividad);
 
-        return json_encode($_POST);
+        foreach ($data['totalRegistros'] as $key => $value) {
+            switch ($value['tipasi_descripcion']) {
+                case 'Tardanza':
+                    $totalTardanza++;
+                    break;
+                case 'Presente':
+                    $totalPresente++;
+                    break;
+                case 'Ausente':
+                    $totalAusentes++;
+                    break;
+            }
+        }
+
+        echo json_encode($data['totalRegistros']);
     }
 }
